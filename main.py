2636c31639d7b5abd2b85fb44b0ecff35b9f3ce3
Khokholeva <svetahoholeva759@gmail.com>
2019-11-18 19:21:01 +0500

    finished 2

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ class FirstForm(QMainWindow):
         self.pushButton = QPushButton('Нажми', self)
         self.pushButton.resize(100, 50)
         self.pushButton.move(350, 350)
-        self.pushButton.setStyleSheet('color: rgb(255, 255, 0); background-color: rgb(0, 0, 0);')
-
+        self.setStyleSheet('color: rgb(255, 255, 0); background-color: rgb(0, 0, 0);')
         self.pushButton.clicked.connect(self.draw_circle)
+
+
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -33,7 +34,7 @@ class FirstForm(QMainWindow):
 
         qp.setBrush(QColor(0, 0, 0))
         qp.drawEllipse(x - 1, y - 1, a + 2, a + 2)
-        r, g, b = randint(0, 256), randint(0, 256), randint(0, 256)
+        r,g, b = randint(0, 256), randint(0, 256), randint(0, 256)
         qp.setBrush(QColor(r, g, b))
         qp.drawEllipse(x, y, a, a)
 
